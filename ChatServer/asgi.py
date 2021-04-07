f3dce5a9405b5473b674eb6d390761a9a273edd5
atharvajainak <61925629+atharvajainak@users.noreply.github.com>
2021-04-07 17:23:35 +0530

    basic chat server using websockets ready

--- ChatServer/asgi.py
+++ ChatServer/asgi.py
@@ -9,11 +9,19 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatServer.settings')
 
 application = ProtocolTypeRouter({
     "http" : get_asgi_application(),
+    "websocket" : AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
--- chat/views.py
+++ chat/views.py
@@ -5,3 +5,9 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index(request):
     return render(request, 'chat/index.html')
+
+@login_required
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name': room_name
+    })
